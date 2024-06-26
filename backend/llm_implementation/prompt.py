ce348ecd4fb469025df6accda73fa23a931d345f
Parthit Patel <pbp38@cornell.edu>
2024-06-25 20:43:04 -0400

    feat: changes the way the rag is queried

--- backend/llm_implementation/prompt.py
+++ backend/llm_implementation/prompt.py
@@ -2,6 +2,15 @@ from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.prompts import PromptTemplate
 
+
+rag_prompt_dict = {
+    'summary':"Return the summary of the congressional bill provided. Return an objective summary of the bill. Answer in less than 60 words. Explain it in simple terms",
+    'concerns':"Return the main concerns of the congressional bill provided. What are the potential implications and down sides of it. Answer in less than 60 words. Explain it in simple terms",
+    'benefits':"Return the benefits of the congressional bill provided. Explain the positive impact it will have. Answer in less than 60 words. Explain it in simple terms"
+    }
+
+# Will not be using these any more. Will be querying for individual context items from the RAG
+
 class BillAnalysis(BaseModel):
     summary: str = Field(description="what does this bill aim to acheive?")
     benefits: str = Field(description="what are the positive impacts of the bill?")
